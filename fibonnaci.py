b4c424ddabac6f60a2da09c419435c55b984fc38
mtsen1 <181283925+mtsen1@users.noreply.github.com>
2024-10-06 13:22:45 -0700

    added functions to find fibonacci sequence/ largest prime

--- fibonnaci.py
+++ fibonnaci.py
@@ -13,11 +13,34 @@ Create a program that generates Fibonacci numbers less than a limit and writes t
 
 Task: Generate the Fibonacci numbers less than 100 and write them to `fibonacci_100.txt`
 """
+import argparse
 
-def some_function(an_argument):
-    # Do something
+def fibonacci(limit):
+    fib = []
+    x = 0
+    y = 1
+    
+    while x < limit:
+        fib.append(x)
+        x, y = y, x + y
+    return fib
 
-    return something
+def write_to_file(fib, file_name):
+     try:
+          with open(file_name, "w") as f:
+                for number in fib:
+                       f.write(f"{number}\n")
+     except: 
+          print("File I/O error")
+
+def main():
+    parser = argparse.ArgumentParser(description="Generate Fibonacci numbers less than a limit.")
+    parser.add_argument("limit", type = int, help="Upper limit for Fibonacci numbers")
+    parser.add_argument("output_file", type = str, help="Output file name")
+
+    args = parser.parse_args()
+    fib = fibonacci(args.limit)
+    write_to_file(fib, args.output_file)
 
 if __name__ == "__main__":
-    # Do stuff here
+    main()
--- largest_prime.py
+++ largest_prime.py
@@ -11,4 +11,38 @@ Write a program that takes a number as an argument, finds the *Fibonacci* number
 Task: Find the largest prime Fibonacci number less that 50000
 """
 
-# You're on your own for this one. Good luck!
+import argparse
+from fibonnaci import fibonacci
+
+def is_prime(num):
+    if num <= 1:
+        return False
+    for i in range(2, num):
+        if num % i == 0:
+            return False
+    else:
+        return True
+
+def big_prime_fib(limit):
+    fib_num = fibonacci(limit)  
+    primes = [num for num in fib_num if is_prime(num)]
+    if primes: 
+        return max(primes)
+
+def main():
+    parser = argparse.ArgumentParser(description= "Find largest prime Fibonacci number")
+    parser.add_argument("limit", type = int, help = "Upper limit for Fibonacci sequence")
+    
+    args = parser.parse_args()
+    
+    largest_prime = big_prime_fib(args.limit)
+    
+    print(largest_prime)
+    
+
+if __name__ == "__main__":
+    main()
+
+
+# the largest prime fibonacci sequence number below 50,000 is 28,657
+ 
